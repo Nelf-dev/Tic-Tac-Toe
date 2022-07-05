354fa8898d844d452acde23a28c6a09132411bc7
Nelson Feng <nelsonfeng98@gmail.com>
2022-07-06 09:13:25 +1000

    Added Draw Condition

--- js/index.js
+++ js/index.js
@@ -10,32 +10,44 @@ $(document).ready(function() {
     console.log( "ready!" );
     console.log($('.grid'));
 
-    const isWin = function (player){
-        if($('#g0').children().attr('class') === player&& $('#g1').children().attr('class') === player && $('#g2').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+    const players = ["X","O"];
+
+
+
+const isWin = function (player){
+    for(let i = 0 ; i < player.length; i++){
+        if($('#g0').children().attr('class') === player[i]&& $('#g1').children().attr('class') === player[i] && $('#g2').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g3').children().attr('class') === player&& $('#g4').children().attr('class') === player && $('#g5').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g3').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g5').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g6').children().attr('class') === player&& $('#g7').children().attr('class') === player && $('#g8').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g6').children().attr('class') === player[i]&& $('#g7').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g0').children().attr('class') === player&& $('#g3').children().attr('class') === player && $('#g6').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g0').children().attr('class') === player[i]&& $('#g3').children().attr('class') === player[i] && $('#g6').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g1').children().attr('class') === player&& $('#g4').children().attr('class') === player && $('#g7').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g1').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g7').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g2').children().attr('class') === player&& $('#g5').children().attr('class') === player && $('#g8').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g2').children().attr('class') === player[i]&& $('#g5').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g0').children().attr('class') === player&& $('#g4').children().attr('class') === player && $('#g8').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g0').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
         }
-        if($('#g2').children().attr('class') === player&& $('#g4').children().attr('class') === player && $('#g6').children().attr('class') === player){
-            console.log(`YOU WIN ${player}`);
+        if($('#g2').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g6').children().attr('class') === player[i]){
+            return(`YOU WIN ${player[i]}`);
+        }
+        if($('#g0').children().length !== 0 && $('#g1').children().length !== 0 && $('#g2').children().length !== 0 && $('#g3').children().length !== 0 && $('#g4').children().length !== 0 && $('#g5').children().length !== 0 && $('#g6').children().length !== 0 && $('#g7').children().length !== 0 && $('#g8').children().length !== 0) {
+            return ('DRAW YOU BOTH SUCK');
         }
     }
+
+}
+
+    
 
     //ALTERNATES FROM BOTH PICTURES WITH MODULO- EVEN OR ODD
 
@@ -57,9 +69,11 @@ $(document).ready(function() {
             $(this).prepend('<img class="O" src="https://www.placecage.com/gif/150/150"/>');
             $('.turn').text('Player 1 turn');
         }
+        // if(isWin() === "draw" && numOfImgInGrid === 9 ){
+        //     console.log("NO BODY WINS YOU ALL SUCK");
+        // }
+        console.log(isWin(players));
 
-        isWin("X");
-        isWin("O");
     });
 
 
