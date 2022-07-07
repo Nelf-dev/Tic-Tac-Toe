22590f6cfc616a43765803a8bf633300b90226e3
Nelson Feng <nelsonfeng98@gmail.com>
2022-07-07 16:59:50 +1000

    condensed win combo code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 
 $(document).ready(function() {
-    console.log( "ready!" );
-    $('.end').hide();
+    $('.end').hide(); // Hides Away end game screen
     const players = ["X","O"];
 
     let scoresX = 0;
     let scoresO = 0;
 
-    if(localStorage.getItem('KeyX')){ // If true there was score. Update scores
+    if(localStorage.getItem('KeyX')){ // Checks local storage for previous logged scores
         scoresX = parseInt(localStorage.getItem('KeyX'));
         $('.X').text(`${scoresX}`);
     }
@@ -17,61 +16,37 @@ $(document).ready(function() {
     }
 
 
-    const isWin = function (player){
-        for(let i = 0 ; i < player.length; i++){
-            if($('#g0').children().attr('class') === player[i]&& $('#g1').children().attr('class') === player[i] && $('#g2').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`);
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g3').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g5').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g6').children().attr('class') === player[i]&& $('#g7').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g0').children().attr('class') === player[i]&& $('#g3').children().attr('class') === player[i] && $('#g6').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g1').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g7').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g2').children().attr('class') === player[i]&& $('#g5').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g0').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g8').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`)
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g2').children().attr('class') === player[i]&& $('#g4').children().attr('class') === player[i] && $('#g6').children().attr('class') === player[i]){
-                $('.endMsg').text(`${player[i]} wins!`);
-                $('.end').fadeIn(600);
-                return(player[i]);
-            }
-            if($('#g0').children().length !== 0 && $('#g1').children().length !== 0 && $('#g2').children().length !== 0 && $('#g3').children().length !== 0 && $('#g4').children().length !== 0 && $('#g5').children().length !== 0 && $('#g6').children().length !== 0 && $('#g7').children().length !== 0 && $('#g8').children().length !== 0) {
-                $('.endMsg').text(`DRAW`)
-                $('.end').fadeIn(600);
-                return ('DRAW');
+    winCombo = [
+        ['#g0','#g1','#g2'],
+        ['#g3','#g4','#g5'],
+        ['#g6','#g7','#g8'], //Horizontal
+
+        ['#g0','#g3','#g6'],
+        ['#g1','#g4','#g7'],
+        ['#g2','#g5','#g8'], //Vertical
+
+        ['#g0','#g4','#g8'], //diagonal
+        ['#g2','#g4','#g6']
+    ];
+    const isWin = function (player){ 
+        for(let i = 0 ; i < player.length; i++){ // Cycles through both X and O into these conditionals
+            for(let j = 0; j < winCombo.length; j++){
+                    if($(`${winCombo[j][0]}`).children().attr('class') === player[i]&& $(`${winCombo[j][1]}`).children().attr('class') === player[i] && $(`${winCombo[j][2]}`).children().attr('class') === player[i]){
+                        $('.endMsg').text(`${player[i]} wins!`);
+                        $('.end').fadeIn(600);
+                        return(player[i]);
+                    }
+                    if($('#g0').children().length !== 0 && $('#g1').children().length !== 0 && $('#g2').children().length !== 0 && $('#g3').children().length !== 0 && $('#g4').children().length !== 0 && $('#g5').children().length !== 0 && $('#g6').children().length !== 0 && $('#g7').children().length !== 0 && $('#g8').children().length !== 0) {
+                        $('.endMsg').text(`DRAW`)
+                        $('.end').fadeIn(600);
+                        return ('DRAW');
+                    }
+                }
             }
         }
     
-    }
-    
         //ALTERNATES FROM BOTH PICTURES WITH MODULO- EVEN OR ODD
-    
-        $('.grid').on('click', function(){ // When click element with class grid
-    
+        $('.grid').on('click', function(){
             let numOfImg = $(this).children().length; //length of each individual grid
             let numOfImgInGrid = $('.grid').children().length + 1; //length of how many img in all grids
             console.log(numOfImgInGrid);
@@ -80,18 +55,16 @@ $(document).ready(function() {
                 return 0;
             }
             if(numOfImgInGrid % 2 === 1){ //If odd 
-                $('<img class="X" src="img/cross.png"/>').hide().prependTo(this).fadeIn(200);
+                $('<img class="X" src="img/cross.png"/>').hide().prependTo(this).fadeIn(200); //Adds picture of X
                 $('.turn').text('Player 2 turn');
             } if (numOfImgInGrid % 2 === 0){ //If Even
-                $('<img class="O" src="img/circle.png"/>').hide().prependTo(this).fadeIn(200);
+                $('<img class="O" src="img/circle.png"/>').hide().prependTo(this).fadeIn(200); //Adds picture of O
                 $('.turn').text('Player 1 turn');
             }
-            console.log(isWin(players));
+            console.log(isWin(players)); //Checks after every click if win combination met
         });
 
         //Button Functions
-
-    
         $('.restart').on('click', function(){ // SCORING
             if(isWin(players) === 'X'){
                 scoresX += 1;
@@ -107,7 +80,7 @@ $(document).ready(function() {
             }
         });
     
-        $('.restart').on('click', function(){ // Restart function
+        $('.restart').on('click', function(){ // Restart button
             $('img').remove();
             $('.turn').text('Please Start player 1');
             $('.end').fadeOut(600);
@@ -116,7 +89,7 @@ $(document).ready(function() {
         $('.reset').on('click', function(){ // Reset function
             $('img').remove();
             $('.turn').text('Please Start player 1');
-            localStorage.clear(); // Clears all local storage scores
+            localStorage.clear(); // Clears all local storage
             scoresX = 0;
             scoresO = 0;
             $('.X').text(`${scoresX}`)
